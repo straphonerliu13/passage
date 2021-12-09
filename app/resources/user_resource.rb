a2ded8eefe2ec45f9cc4d2cd228faae02faab3dd
anonymous anonymous <s.traphonerliu@gmail.com>
2021-12-09 07:48:01 +0000

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,21 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :first_name, :string
+  attribute :last_name, :string
+  attribute :email_address, :string
+  attribute :password, :string
+  attribute :home_city, :string
+  attribute :home_state, :string
+  attribute :home_country, :string
+  attribute :user_image, :string
+  attribute :user_bio, :string
+  attribute :user_type_id, :integer
+  attribute :demographic_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end

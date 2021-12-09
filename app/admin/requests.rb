99bd2ee7ca67d60f566f119cd3eef68593b4912f
anonymous anonymous <s.traphonerliu@gmail.com>
2021-12-09 07:44:37 +0000

    Whitelist columns in Request for ActiveAdmin

--- app/admin/requests.rb
+++ app/admin/requests.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Request do
 
+ permit_params :request_name, :request_description, :status_id, :approver_id, :requestor_id, :exhibit_id, :attraction_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #

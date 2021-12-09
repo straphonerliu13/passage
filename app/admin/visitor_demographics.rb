11f0a377e2b1afea6c8fe9222328e98cc5a8afeb
anonymous anonymous <s.traphonerliu@gmail.com>
2021-12-09 07:44:30 +0000

    Whitelist columns in VisitorDemographic for ActiveAdmin

--- app/admin/visitor_demographics.rb
+++ app/admin/visitor_demographics.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register VisitorDemographic do
 
+ permit_params :demographic_name, :demographic_description
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #

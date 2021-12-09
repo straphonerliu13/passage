4e5a3de72821af26e02691e7024596ca04037d65
anonymous anonymous <s.traphonerliu@gmail.com>
2021-12-09 07:44:01 +0000

    Whitelist columns in Attraction for ActiveAdmin

--- app/admin/attractions.rb
+++ app/admin/attractions.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Attraction do
 
+ permit_params :attraction_name, :attraction_location, :attraction_description, :attraction_image, :status_id, :phone_number, :email_address, :hours_of_operation, :cost_of_entry, :attraction_category_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #

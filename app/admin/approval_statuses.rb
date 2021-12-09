ee2de310d35a6c71a94ef4b8ae01e5c6451c77fa
anonymous anonymous <s.traphonerliu@gmail.com>
2021-12-09 07:44:33 +0000

    Whitelist columns in ApprovalStatus for ActiveAdmin

--- app/admin/approval_statuses.rb
+++ app/admin/approval_statuses.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ApprovalStatus do
 
+ permit_params :status_name
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
